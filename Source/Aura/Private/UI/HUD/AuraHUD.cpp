a97d5c93e7063055ddc955a86955d05d222e7f72
Muhammad Ahsan <muhammad2009ahsan@gmail.com>
2024-08-29 16:34:43 +0500

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+// CopyRight Aura
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widget/AuraUserWidget.h"
+#include "Blueprint/UserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+}
