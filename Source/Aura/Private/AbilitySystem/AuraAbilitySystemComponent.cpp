2a69f253eeba74fb1f6a3274a7a38b5fa404bd28
Muhammad Ahsan <muhammad2009ahsan@gmail.com>
2024-09-05 19:13:37 +0500

    Broadcasting Effect Asset Tags

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -13,10 +13,5 @@ void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Ability
 {
 	FGameplayTagContainer TagContainer;
 	EffectSpec.GetAllAssetTags(TagContainer);
-	for (const FGameplayTag& Tag : TagContainer)
-	{
-		// TODO:Broadcast The Tag to The WidgetContoller
-		const FString Msg = FString::Printf(TEXT("GE Tag: %s"),*Tag.ToString());
-		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Blue, Msg);
-	}
+	EffectAssetTags.Broadcast(TagContainer);
 }
