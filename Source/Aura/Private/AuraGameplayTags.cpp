6b189496ec595806bd6580be76923ad06e093fc9
Muhammad Ahsan <muhammad2009ahsan@gmail.com>
2025-06-17 15:40:02 +0500

    Minion Count

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -113,6 +113,7 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 
 	GameplayTags.Abilities_Attack = UGameplayTagsManager::Get().AddNativeGameplayTag((FName("Abilities.Attack")), FString("Attack Ability Tag"));
 
+	GameplayTags.Abilities_Summon = UGameplayTagsManager::Get().AddNativeGameplayTag((FName("Abilities.Summon")), FString("Summon Ability Tag"));
 
 
 	/*
